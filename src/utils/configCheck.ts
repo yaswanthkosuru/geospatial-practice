67e8e372d436178d315b6abef5c0e98f726bb54a
yaswanth kosuru <yaswanthkosuru999@gmail.com>
2024-08-03 07:50:10 +0530

    feature:added nodemon to restart server after save

--- src/utils/configCheck.ts
+++ src/utils/configCheck.ts
@@ -2,7 +2,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 export function areEnvVariablesAvailable(...values: string[]): boolean {
   for (const value of values) {
-    const envvariable = console.log(process.env[value], "value");
     if (!process.env[value]) {
       console.error(`Please provide ${value}`);
       return false;
