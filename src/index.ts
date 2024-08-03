90403854f057d8b0e0847eaa433622496c72ea2e
yaswanth kosuru <yaswanthkosuru999@gmail.com>
2024-08-03 17:52:05 +0530

    suport for vercel

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,37 @@
-import express, { Request, Response, NextFunction } from "express";
-import {
-  connectToDatabase,
-  closeConnection,
-} from "./utils/database/connectionTomongo";
-import restaurantRoutes from "./routes/restaurantRoutes";
-import userRoutes from "./routes/userRoutes";
+import express, { Request, Response, NextFunction } from 'express';
+import { connectToDatabase, closeConnection } from './utils/database/connectionTomongo';
+import restaurantRoutes from './routes/restaurantRoutes';
+import userRoutes from './routes/userRoutes';
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
 let db: any;
+connectToDatabase();
+app.use((req, res, next) => {
+	console.log(req.path, req.method);
+	next();
+});
 
-const startServer = async () => {
-  try {
-    db = await connectToDatabase();
+app.get('/', (req, res) => {
+	res.json({ message: 'server running succesfully' });
+});
+app.use('/api', restaurantRoutes);
+app.use('/api', userRoutes);
 
-    app.use((req, res, next) => {
-      console.log(req.path, req.method);
-      next();
-    });
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
+});
 
-    app.get("/", (req, res) => {
-      res.json({ message: "server running succesfully" });
-    });
-    app.use("/api", restaurantRoutes);
-    app.use("/api", userRoutes);
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
 
-    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-      console.error(err.stack);
-      res.status(500).send("Something broke!");
-    });
+process.on('SIGINT', async () => {
+	await closeConnection();
+	process.exit(0);
+});
 
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
-    });
-
-    process.on("SIGINT", async () => {
-      await closeConnection();
-      process.exit(0);
-    });
-  } catch (err) {
-    console.error("Failed to start server", err);
-    process.exit(1);
-  }
-};
-
-startServer();
+export default app;
